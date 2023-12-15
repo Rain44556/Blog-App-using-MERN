96f39236e16eca7f1862709bfc0fdc17ca5ce744
Rain44556 <bristy44556@gmail.com>
2023-12-15 21:32:42 +0600

    logo and button add in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {AppBar, Toolbar, styled, Typography} from '@mui/material';
+import {AppBar, Toolbar, Box, Button, styled, Typography} from '@mui/material';
+import {brown} from '@mui/material/colors'
 
 const Img = styled('img')({
-    width: 80,
-   padding: 5, 
+    width: 100,
+   padding: 5,
+   borderRadius: 29, 
 });
+
 
 const Header = () => {
     const imgUrl = "https://img.freepik.com/free-photo/toy-bricks-table_144627-48267.jpg?w=996&t=st=1702643534~exp=1702644134~hmac=2a2e3b23c88fb8b7f3cad85865c88950544b4bfd99a4c5fdfdf1d31794c41525";
     return (
-        <AppBar>
+        <AppBar sx={{backgroundColor: "rgb(238,209,174)"}}>
             <Toolbar>
                 <Img src ={imgUrl} alt = "blog"/>
-                <Typography>App</Typography>
+                <Typography variant='h6'>ApP</Typography>
+           <Box display="flex" marginLeft="auto">
+            <Button  sx={{margin: 2, borderRadius: 1, backgroundColor: brown[400]}} variant='contained' >Login</Button>
+            <Button sx={{margin: 2,  borderRadius: 1, backgroundColor: brown[400]}} variant='contained'>Signup</Button>
+           </Box>
             </Toolbar>
             </AppBar>
     );
