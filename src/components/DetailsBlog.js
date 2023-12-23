14112f1f3924242d941aca1b96f198d4ef7c1481
Rain44556 <bristy44556@gmail.com>
2023-12-23 12:55:32 +0600

    blogs details connect with backend succefully

--- src/components/DetailsBlog.js
+++ src/components/DetailsBlog.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const DetailsBlog = () => {
+    const [blog, setBlog] = useState();
+    const id = useParams().id;
+    console.log(id);
+    const detailsFetch = async() =>{
+        const res = await axios.get(`http://localhost:5000/api/blog/${id}`).catch((err)=>console.log(err));
+        const data = await res.data;
+        return data;
+    
+    }
+    useEffect(()=>{
+        detailsFetch().then(data => setBlog(data.blog))
+    }, [id]);
+    console.log(blog);
+
+
     return (
         <div>
             details
